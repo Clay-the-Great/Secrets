584717ed8821b86bd1694275598e31c5e983810e
Clay-the-Great <106385441+Clay-the-Great@users.noreply.github.com>
2022-07-15 20:44:38 +0800

    Update to hashing.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -42,7 +43,7 @@ app.get("/login", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -56,7 +57,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const userName = req.body.username;
-  const passWord = req.body.password;
+  const passWord = md5(req.body.password);
   User.findOne({email: userName}, function(err, foundUser){
     if(err){
       console.log(err);
